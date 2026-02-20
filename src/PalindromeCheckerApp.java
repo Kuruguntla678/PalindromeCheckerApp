971f45b5c60035e464d4c6d559f6f3e04c541ca6
Kuruguntla678 <kuruguntlacharankarthikreddy@gmail.com>
2026-02-20 11:14:54 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,7 @@
 public class PalindromeCheckerApp {
    public  static void main(String[] args) {
-       public class PalindromeCheckerApp {
-
-           static final String APP_VERSION = "1.0.0";
-
-           public static void main(String[] args) {
-
-               System.out.println("=================================");
-               System.out.println("      PALINDROME CHECKER APP     ");
-               System.out.println("=================================");
-
-               System.out.println("Version: " + APP_VERSION);
-               System.out.println("Author: Your Name");
-
-               System.out.println("---------------------------------");
-               System.out.println("Welcome to the Palindrome Checker!");
-               System.out.println("---------------------------------");
 
 
-           }
-       }
+
     }
 }

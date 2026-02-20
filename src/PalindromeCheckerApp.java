caebe22e1d20295dfb0daef678663f2ae569c72f
Kuruguntla678 <kuruguntlacharankarthikreddy@gmail.com>
2026-02-20 11:25:04 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,21 @@
 public class PalindromeCheckerApp {
    public  static void main(String[] args) {
+       String word = "madam";
 
+       boolean isPalindrome = true;
 
+       for (int i = 0; i < word.length() / 2; i++) {
+           if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+               isPalindrome = false;
+               break;
+           }
+       }
+
+       if (isPalindrome) {
+           System.out.println("The word \"" + word + "\" is a Palindrome.");
+       } else {
+           System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+       }
 
     }
 }

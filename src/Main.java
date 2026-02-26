c3835f027ea2114446e85bfdaae09961adeb4366
Kuruguntla678 <kuruguntlacharankarthikreddy@gmail.com>
2026-02-26 22:05:20 +0530

    UC5: Palindrome ignoring special characters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
+import java.util.Stack;
 
-public class PalindromeCheckerApp {
+    public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
@@ -12,10 +13,16 @@ public class PalindromeCheckerApp {
         // UC3
         checkPalindromeUsingLoop();
 
+        // UC4
+        checkPalindromeUsingTwoPointers();
+
+        // UC5
+        checkPalindromeUsingStack();
+
         System.out.println("Application is ready for palindrome processing...");
     }
 
-    // UC1 logic
+    // ===== UC1 =====
     public static void displayWelcomeMessage() {
         System.out.println("=========================================");
         System.out.println("        PALINDROME CHECKER APP           ");
@@ -25,7 +32,7 @@ public class PalindromeCheckerApp {
         System.out.println("=========================================");
     }
 
-    // UC2 logic
+    // ===== UC2 =====
     public static void checkHardcodedPalindrome() {
         String word = "madam";
         String reversed = new StringBuilder(word).reverse().toString();
@@ -37,13 +44,57 @@ public class PalindromeCheckerApp {
         }
     }
 
-    // UC3 logic
+    // ===== UC3 =====
     public static void checkPalindromeUsingLoop() {
         String word = "racecar";
         String reversed = "";
 
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("Result: " + word + " is a palindrome ✅");
+        } else {
+            System.out.println("Result: " + word + " is NOT a palindrome ❌");
+        }
+    }
+
+    // ===== UC4 =====
+    public static void checkPalindromeUsingTwoPointers() {
+        String word = "level";
+        int left = 0;
+        int right = word.length() - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Result: " + word + " is a palindrome ✅");
+        } else {
+            System.out.println("Result: " + word + " is NOT a palindrome ❌");
+        }
+    }
+
+    // ===== UC5 =====
+    public static void checkPalindromeUsingStack() {
+        String word = "deed";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
         }
 
         if (word.equals(reversed)) {
